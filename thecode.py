91d120bc77124a8b0e83f6d31795d86842d8866b
Phoebe Ireland <pireland2016@gmail.com>
2021-01-30 20:34:56 +0000

    add the code that makes the dictionary with the info

--- thecode.py
+++ thecode.py
@@ -1,14 +1,25 @@
 import tkinter as tk
 import random
 
-food_dictonary = {}
+overall_food_dict = {}
 
 with open("list_of_countries_and_recipes.csv", 'r') as recipe_file:
-    line = recipe_file.readline()
-    print(line)
+    recipe_file.readline()
+    lines = recipe_file.readlines()
+    for line in lines:
+        continent, country, recipe_name, recipe_link = line.split(',')
 
-print(food_dictonary)
+        if continent not in overall_food_dict:
+            overall_food_dict[continent]= {}
 
+        if country not in overall_food_dict[continent]:
+            overall_food_dict[continent][country]= {}
+
+        overall_food_dict[continent][country][recipe_name]= recipe_link
+
+#print(overall_food_dict['North America']['Cuba']["Cuban Sandwich"])
+
+#choice_overall_dict = ""
 
 root = tk.Tk()
 root.title("Geo-Foodie")
@@ -21,7 +32,7 @@ label.pack()
 
 
 def write_slogan():
-    choice = random.choice(food_dictonary)
+    choice = random.choice()
     # print(choice)
     label.config(text=choice)
 
