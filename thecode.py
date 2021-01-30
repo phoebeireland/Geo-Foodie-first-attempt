b8d760370275b86054904cb9a4985025eb709d1d
Phoebe Ireland <pireland2016@gmail.com>
2021-01-30 18:09:45 +0000

    add base code for picking a random selection from a list

---
+++ thecode.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+
+import random
+
+mylist = [
+    "paint with Bob Ross",
+    "picnic",
+    "massage",
+    "charcuterie board",
+    "movie night",
+    "blindfolded dinner",
+]
+
+
+root = tk.Tk()
+root.title("Date Night")
+frame = tk.Frame(root)
+frame.pack()
+
+
+label = tk.Label(root, fg="dark green")
+label.pack()
+
+
+def write_slogan():
+    choice = random.choice(mylist)
+    # print(choice)
+    label.config(text=choice)
+
+
+tk.Button(frame, text="QUIT", fg="red", command=quit).pack(side=tk.LEFT)
+
+slogan = tk.Button(frame, text="Press me", command=write_slogan)
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
